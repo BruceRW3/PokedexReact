aa4cb11c4eb25db3bf125823a759deb909c70634
BruceRW3 <brwiggleston@gmail.com>
2023-05-19 16:52:13 -0400

    added client

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const pokeQuery = gql`
+  query samplePokeAPIquery {
+    gen3_species: pokemon_v2_pokemonspecies(
+      where: { pokemon_v2_generation: { name: {} } }
+      order_by: { id: asc }
+    ) {
+      name
+      id
+    }
+  }
+`;
+
+function App() {
+  const { loading, error, data } = useQuery(pokeQuery);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <>
+      <li>Pokemon</li>
+      {data.gen3_species.map((species) => (
+        <li key={species.id} value={species.name}>
+          {species.name}
+        </li>
+      ))}
+    </>
+  );
+}
+
+export default App;
